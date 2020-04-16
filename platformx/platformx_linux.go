172f6b1ee452d7ef555ce6f36bcd3e72ede804ef
Marco Aresu <marco.aresu@lastminute.com>
2020-04-16 16:19:27 +0200

    final playbook

--- platformx/platformx_linux.go
+++
@@ -1,4 +0,0 @@
-package platformx
-
-func maybeEmitWarning() {
-}
--- version/version.go
+++
@@ -1,6 +0,0 @@
-// Package version contains ndt-server version
-package version
-
-// Version is the version of ndt-server. You override this at compile time
-// using `-ldflags "-X variable=value"` facility.
-var Version string
